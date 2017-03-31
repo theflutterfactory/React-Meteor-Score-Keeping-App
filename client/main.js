ad4a727bc6962f5b7fa0d84a1497856be2f3f145
Julian Currie <julian.currie@gmail.com>
2017-03-31 19:44:16 +0700

    created method to render players with dummy data

--- client/main.js
+++ client/main.js
@@ -1,3 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {Meteor} from 'meteor/meteor';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Meteor } from "meteor/meteor";
+
+const players = [
+  {
+    _id: "1",
+    name: "John",
+    score: 99
+  },
+  {
+    _id: "2",
+    name: "Jacob",
+    score: -1
+  },
+  {
+    _id: "3",
+    name: "Joe",
+    score: -12
+  }
+];
+
+const renderPlayers = function(playerList) {
+  return playerList.map(function(player) {
+    return <p key={player._id}>{player.name} has {player.score} points.</p>;
+  });
+};
+
+Meteor.startup(() => {
+  let title = "Score Keep";
+  let name = "Julian";
+  let jsx = (
+    <div>
+      <h1>{title}</h1>
+      <p>Hello {name}!</p>
+      {renderPlayers(players)}
+    </div>
+  );
+  ReactDOM.render(jsx, document.getElementById("app"));
+});
