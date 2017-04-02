a02f90753ba148b76271dba19b110f9f43593342
Julian Currie <julian.currie@gmail.com>
2017-04-02 20:37:36 +0700

    added css styles & classes for body & Title bar

--- imports/ui/TitleBar.js
+++ imports/ui/TitleBar.js
@@ -3,9 +3,11 @@ import React from "react";
 export default class TitleBar extends React.Component {
   render() {
     return (
-      <div>
-        <h1>{this.props.titleProp}</h1>
-        <h3>{this.props.subtitleProp}</h3>
+      <div className="title-bar">
+        <div className="wrapper">
+          <h1>{this.props.titleProp}</h1>
+          <h3>{this.props.subtitleProp}</h3>
+        </div>
       </div>
     );
   }
