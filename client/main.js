acd1e892914a9209ea9099b9f35de3498db58dc4
Julian Currie <julian.currie@gmail.com>
2017-04-02 15:48:59 +0700

    Added a Title Bar component

--- client/main.js
+++ client/main.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import { Meteor } from "meteor/meteor";
 import { Tracker } from "meteor/tracker";
 import { Players } from "./../imports/api/players";
+import TitleBar from "./../imports/ui//TitleBar";
 
 const renderPlayers = playerList =>
   playerList.map(player => (
@@ -48,12 +49,10 @@ Meteor.startup(() => {
   Tracker.autorun(() => {
     let players = Players.find().fetch();
     let title = "Score Keep";
-    let name = "Julian";
 
     let jsx = (
       <div>
-        <h1>{title}</h1>
-        <p>Hello {name}!</p>
+        <TitleBar />
         {renderPlayers(players)}
         <form onSubmit={handleSubmit}>
           <input type="text" name="playerName" placeholder="Player name" />
---
+++ imports/ui/TitleBar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class TitleBar extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Score Keep</h1>
+      </div>
+    );
+  }
+}
