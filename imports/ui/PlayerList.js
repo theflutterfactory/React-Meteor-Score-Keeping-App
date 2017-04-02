062110f668813bba54d71e5cab788ba751ced2bb
Julian Currie <julian.currie@gmail.com>
2017-04-02 18:29:24 +0700

    show Empty list message when list is empty

--- imports/ui/PlayerList.js
+++ imports/ui/PlayerList.js
@@ -3,9 +3,13 @@ import Player from "./Player";
 
 export default class PlayerList extends React.Component {
   renderPlayers() {
-    return this.props.players.map(player => {
-      return <Player key={player._id} player={player} />;
-    });
+    if (this.props.players.length === 0) {
+      return <p>The Players List is Empty</p>;
+    } else {
+      return this.props.players.map(player => {
+        return <Player key={player._id} player={player} />;
+      });
+    }
   }
 
   render() {
