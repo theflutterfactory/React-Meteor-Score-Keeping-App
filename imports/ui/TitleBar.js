9d545c3f3903b5d41298c7d9011f020b437be563
Julian Currie <julian.currie@gmail.com>
2017-04-03 18:00:07 +0700

    Added more styles for Player, subtitle, inputs, buttons

--- imports/ui/TitleBar.js
+++ imports/ui/TitleBar.js
@@ -6,7 +6,7 @@ export default class TitleBar extends React.Component {
       <div className="title-bar">
         <div className="wrapper">
           <h1>{this.props.titleProp}</h1>
-          <h3>{this.props.subtitleProp}</h3>
+          <h3 className="title-bar__subtitle">{this.props.subtitleProp}</h3>
         </div>
       </div>
     );
